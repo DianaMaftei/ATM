77773060de4e8d6decff66b48e0a89cdf3cf1f5f
Dee-Dee <Dee-Dee@Pishcotzel>
2016-05-17 21:32:22 +0300

    new test

--- ATM/src/main/AtmMain.java
+++ ATM/src/main/AtmMain.java
@@ -1,3 +1,4 @@
+//check mods
 package main;
 
 import java.util.Scanner;
---
+++ Library/src/main/Book.java
@@ -0,0 +1,60 @@
+package main;
+
+public class Book {
+	
+	private final String NAME;
+	private final String AUTHOR;
+	private final String PUBLISHING_HOUSE;
+	private final String ISBN;
+	private final String YEAR;
+	
+	//exemplare existente in baza de date / disponibile pt imprumut
+	private int existing;
+	private int available;
+		
+	
+	public Book(String name, String author, String publishingHouse, String isbn, String year) {
+		super();
+		this.NAME = name;
+		this.AUTHOR = author;
+		this.PUBLISHING_HOUSE = publishingHouse;
+		this.ISBN = isbn;
+		this.YEAR = year;
+	}
+	
+	public String getName() {
+		return NAME;
+	}
+	public String getAuthor() {
+		return AUTHOR;
+	}
+	public String getPublishingHouse() {
+		return PUBLISHING_HOUSE;
+	}
+	public String getIsbn() {
+		return ISBN;
+	}
+	public String getYEAR() {
+		return YEAR;
+	}
+
+	public int getExisting() {
+		return existing;
+	}
+
+	public void setExisting(int existing) {
+		this.existing = existing;
+	}
+
+	public int getAvailable() {
+		return available;
+	}
+
+	public void setAvailable(int available) {
+		this.available = available;
+	}
+	
+	
+	
+
+}
